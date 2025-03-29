db3efcb0eec4828c2fd70a0af0cfd0d269b73306
Daniel Ise <ise.daniel@gmail.com>
2025-03-29 10:06:43 -0300

    clase 02 estructuras started

---
+++ src/13-estructuras/code/clase0329/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stack> // permite manejar pilas
+
+int main()
+{
+    int n; // creo un entero
+    std::stack<int> p;
+
+    p.push(n); // agrego un elemento a la pila
+
+    std::cout << "Hello world!\n";
+    return 0;
+}
