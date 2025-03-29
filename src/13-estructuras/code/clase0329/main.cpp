fd2e9a064f8f30a9d029abcc0f14606865c8926e
Daniel Ise <ise.daniel@gmail.com>
2025-03-29 11:00:33 -0300

    segunda clase done

--- src/13-estructuras/code/clase0329/main.cpp
+++ src/13-estructuras/code/clase0329/main.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
-#include <stack> // permite manejar pilas
+// permite manejar pilas
+#include <stack>
+
+// pila es estructura dinamica
+// no necesito saber cuantos elementos voy a tener
 
 int main()
 {
-    int n; // creo un entero
+    // creo un entero
+    int n;
+
+     // creo una pila<tipo>
     std::stack<int> p;
 
-    p.push(n); // agrego un elemento a la pila
+    // agrego un elemento a la pila
+    p.push(n);
 
-    std::cout << "Hello world!\n";
+    // agrego varios elementos
+    p.push(10);
+    p.push(20);
+    p.push(30);
+
+    // itero para imprimir
+    while (!p.empty()) {
+            // imprimo elemento al tope
+            std::cout << p.top() << '\n';
+            // saco elemento del tope
+            p.pop();
+    }
+
+    // ingresamos manual
+    std::cin >> n;
+    while (n!=0) {
+        p.push(n);
+        std::cin>>n;
+    }
+
+    // itero para imprimir todo de nuevo
+    while (!p.empty()) {
+            // imprimo elemento al tope
+            std::cout << p.top() << '\n';
+            // saco elemento del tope
+            p.pop();
+    }
+
     return 0;
 }
