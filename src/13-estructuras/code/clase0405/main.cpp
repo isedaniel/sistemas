3ba74d43011b16942653d038091150edbc067f07
Daniel Ise <ise.daniel@gmail.com>
2025-04-05 11:17:44 -0300

    clase 3 done

--- src/13-estructuras/code/clase0405/main.cpp
+++ src/13-estructuras/code/clase0405/main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
 using namespace std;
 
+// creamos estructura
+struct Person
+{
+    // declaramos sus campos
+    string name;
+    int age;
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    // declaramos una instancia de la estructura
+    Person p;
+    // asignamos valores
+    p.name = "Adriana";
+    p.age = 22;
+
+    // imprimimos
+    cout << p.name << '\n' << p.age << '\n';
+
+    // creamos pila de personas
+    stack<Person> persons;
+
+    // agregamos la persona creada
+    persons.push(p);
+
+    cout << persons.top().name << '\n';
+
+    // agregamos personas hasta ingresar 0
+    cin >> p.name;
+    while (p.name != "0")
+    {
+        cin >> p.age;
+        persons.push(p);
+        cin >> p.name;
+    }
+
+    // mostramos personas apiladas
+    while (!persons.empty())
+    {
+        cout << persons.top().name << '\n';
+        cout << lastAge << '\n';
+        persons.pop();
+    }
+
     return 0;
 }
