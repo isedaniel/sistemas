784aa3d3a65595b9f5a1edaa47585435cc7259ba
Daniel Ise <ise.daniel@gmail.com>
2025-04-12 10:27:31 -0300

    con stack

--- src/13-estructuras/code/clase0412/main.cpp
+++ src/13-estructuras/code/clase0412/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
+struct Person
+{
+    string name;
+    int age;
+
+    Person(string s, int i)
+    {
+        this->name = s;
+        this->age = i;
+    }
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    string auxName = "Juan";
+    int auxAge = 10;
+
+    stack<Person> Persons;
+    Persons.emplace(auxName, auxAge);
+
+    Person p("Pedro", 11);
+    Persons.push(p);
+
+    int size = Persons.size();
+    for (int i = 0; i < size; i++)
+    {
+        cout << Persons.top().name << ' ' << Persons.top().age << '\n';
+        Persons.pop();
+    }
+
     return 0;
 }
