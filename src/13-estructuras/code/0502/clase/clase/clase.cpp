d24fd5cea97e2a2858e56b7642949f6a1dde18e0
Daniel Ise <ise.daniel@gmail.com>
2025-04-28 19:10:12 -0300

    tests for using visual studio

--- src/13-estructuras/code/0502/clase/clase/clase.cpp
+++ src/13-estructuras/code/0502/clase/clase/clase.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <list>
+
+static void printList(const std::list<int>& l)
+{
+	std::cout << '{' << ' ';
+
+	// size and iteration count for comma printing
+	size_t size{ l.size() };
+	int i{ 1 };
+
+	for (int e : l)
+	{
+		std::cout << e;
+
+		// print commas until last element
+		i++;
+		if (i < size)
+		{
+			std::cout << ',' << ' ';
+		}
+	}
+	std::cout << ' ' << '}' << '\n';
+}
 
 int main()
 {
-	std::cout << "Hello, world!";
+	std::cout << "Hello, world!\n";
+	std::list<int> lA{ 1,2,3 };
+
+	lA.push_front(12);
+	printList(lA);
 }
