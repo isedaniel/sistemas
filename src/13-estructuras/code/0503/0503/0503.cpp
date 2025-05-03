aae3d3e4cb3666348bcc97dc0c5e747fccbb63d6
Daniel Ise <ise.daniel@gmail.com>
2025-05-03 12:45:36 -0300

    todos los ejercicios done

--- src/13-estructuras/code/0503/0503/0503.cpp
+++ src/13-estructuras/code/0503/0503/0503.cpp
@@ -1,13 +1,70 @@
 #include <iostream>
-#include <list>
 
+/*
+1) Ingresar números hasta ingresar 0 y luego mostrar los números en orden inverso al ingresado. (Usando pilas)
+Ejemplo: 100, 200, 300, 0 -> 300, 200, 100
+*/
+
+#include<stack>
+
+static void ejercicio1()
+{
+	// creo stack de enteros
+	std::stack<int> s;
+
+	// ingreso números hasta el 0
+	int n{};
+	std::cin >> n;
+	while (n != 0)
+	{
+		s.push(n);
+		std::cin >> n;
+	}
+
+	// imprimo todo orden Last In First Out (LIFO)
+	while (!s.empty())
+	{
+		std::cout << s.top() << ' ';
+		s.pop();
+	}
+}
+
+/*
+2) Ingresar números hasta ingresar 0 y luego mostrar los números en el orden  ingresado. (Usando colas)
+Ejemplo: 100, 200, 300, 0 -> 100, 200, 300
+*/
+
+#include<queue>
+
+static void ejercicio2()
+{
+	std::queue<int> q{};
+
+	// tomamos valores hasta ingresar 0
+	int n{};
+	std::cin >> n;
+	while (n != 0)
+	{
+		q.push(n);
+		std::cin >> n;
+	}
+
+	// imprimimos valores orden First In First Out (FIFO)
+	while (!q.empty())
+	{
+		std::cout << q.front() << ' ';
+		q.pop();
+	}
+}
 
 /*
 3) Ingresar números hasta ingresar 0 y luego mostrar los números pares en orden ascendente y luego mostrar los impares en orden descendente. (Usando Listas)
 Ejemplo: 1, 3, 4, 2, 5, 7, 9, 8, 0 -> 2, 4, 8, 9, 7, 5, 3, 1
 */
 
-int main()
+#include <list>
+
+static void ejercicio3()
 {
 	std::list<int> even, odd;
 
@@ -42,4 +99,56 @@ int main()
 	{
 		std::cout << i << ' ';
 	}
+}
+
+/*
+4) Ingresar números hasta ingresar 0 y mostrando cada vez todos los números ordenados. (Usando arboles)
+Ejemplo: 10 -> 10; 4 -> 4, 10; 8 -> 4, 8, 10; 0
+*/
+
+#include <set>
+
+static void ejercicio4()
+{
+	std::set<int> s;
+
+	int n{ 1 };
+	std::cin >> n;
+	while (n != 0)
+	{
+		s.insert(n);
+		for (int e : s)
+		{
+			std::cout << e << ' ';
+		}
+		std::cout << '\n';
+		std::cin >> n;
+	}
+}
+
+/*
+5) Igual al 4 pero imprimiendo al revés
+*/
+
+static void ejercicio5()
+{
+	std::set<int> s;
+
+	int n{ 1 };
+	std::cin >> n;
+	while (n != 0)
+	{
+		s.insert(n);
+		for (auto it=s.rbegin(); it!=s.rend(); it++)
+		{
+			std::cout << *it << ' ';
+		}
+		std::cout << '\n';
+		std::cin >> n;
+	}
+}
+
+int main()
+{
+	ejercicio1();
 }
