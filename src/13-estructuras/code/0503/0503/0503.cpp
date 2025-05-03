b691e2361ea8e4823524e257237ee6821c632ce8
Daniel Ise <ise.daniel@gmail.com>
2025-05-03 09:47:59 -0300

    ejercicio 3 done

--- src/13-estructuras/code/0503/0503/0503.cpp
+++ src/13-estructuras/code/0503/0503/0503.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <list>
+
+
+/*
+3) Ingresar números hasta ingresar 0 y luego mostrar los números pares en orden ascendente y luego mostrar los impares en orden descendente. (Usando Listas)
+Ejemplo: 1, 3, 4, 2, 5, 7, 9, 8, 0 -> 2, 4, 8, 9, 7, 5, 3, 1
+*/
 
 int main()
 {
-	std::cout << "Hello, world!\n";
+	std::list<int> even, odd;
+
+	// ingresamos numeros, van pares a lista even, impares a lista odd
+	int n{ 1 };
+	std::cin >> n;
+	while (n != 0)
+	{
+		if (n % 2 == 0)
+		{
+			even.push_back(n);
+		}
+		else
+		{
+			odd.push_back(n);
+		}
+		std::cin >> n;
+	}
+
+	// ordenamos listas, odd de mayor a menor
+	even.sort();
+	odd.sort();
+	odd.reverse();
+
+	// imprimimos
+	for (int i : even)
+	{
+		std::cout << i << ' ';
+	}
+
+	for (int i : odd)
+	{
+		std::cout << i << ' ';
+	}
 }
