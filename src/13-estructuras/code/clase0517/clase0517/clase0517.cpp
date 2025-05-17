fc53d34581fdc3f85ab759c5870586c6b7127fd1
Daniel Ise <ise.daniel@gmail.com>
2025-05-17 08:42:31 -0300

    clase estructura de datos started

---
+++ src/13-estructuras/code/clase0517/clase0517/clase0517.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+//#include <cstdlib>
+
+/*
+    Definimos una estructura que tiene un dato entero y un puntero al siguiente nodo.
+*/
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    // creo un puntero a un nodo, lo llamo pila y apunto a puntero nulo
+    Node* stack = nullptr; 
+}
