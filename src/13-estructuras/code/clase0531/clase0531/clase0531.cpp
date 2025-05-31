f8a7a10d7038a84b50e68566a4ee29fb9a0e210a
Daniel Ise <ise.daniel@gmail.com>
2025-05-31 12:08:40 -0300

    implementacion para listas de string done

--- src/13-estructuras/code/clase0531/clase0531/clase0531.cpp
+++ src/13-estructuras/code/clase0531/clase0531/clase0531.cpp
@@ -122,15 +122,19 @@ static void delete_first(List& l)
     }
     else
     {
+        Node* aux = l.first;
         l.first = l.first->next;
-        free(l.first->prev);
+        free(aux);
 
         // Si first es nulo, no tenemos mas elementos
         if (l.first == nullptr) l.last = nullptr;
     }
 }
 
-int main()
+/*
+    Primera iteracion
+*/
+static void first_iteration()
 {
     /*
         Creamos lista: 11, 12, 13
@@ -159,4 +163,137 @@ int main()
     */
     delete_first(l);
     delete_first(l);
+}
+
+
+/*
+    Nodo de strings
+*/
+struct StringNode
+{
+    StringNode* next;
+    StringNode* prev;
+    std::string value;
+
+    StringNode(const std::string& s) : value(s), next(nullptr), prev(nullptr) {}
+};
+
+
+/*
+    Creamos nodo con new
+*/
+static StringNode* create_node(const std::string& value)
+{
+    return new StringNode(value);
+}
+
+
+/*
+    Lista de nodo de strings
+*/
+struct StringList
+{
+    StringNode* first;
+    StringNode* last;
+
+    // Constructor predeterminado
+    StringList(): first(nullptr), last(nullptr) {}
+};
+
+
+/*
+    Insertamos nodo nuevo adelante de la lista
+*/
+static void insert_first(StringList& l, const std::string& value)
+{
+    StringNode* n = create_node(value);
+    if (l.first == nullptr)
+    {
+        l.first = n;
+        l.last = n;
+    }
+    else
+    {
+        n->next = l.first;
+        l.first->prev = n;
+        l.first = n;
+    }
+}
+
+
+/*
+    Insertamos nodo nuevo al final de la lista
+*/
+static void insert_last(StringList& l, const std::string& value)
+{
+    StringNode* n = create_node(value);
+    if (l.last == nullptr)
+    {
+        l.first = n;
+        l.last = n;
+    }
+    else
+    {
+        n->prev = l.last;
+        l.last->next = n;
+        l.last = n;
+    }
+}
+
+
+/*
+    Imprimimos lista de string
+*/
+static void print_list(StringList& l)
+{
+    StringNode* aux = l.first;
+    while (aux != nullptr)
+    {
+        std::cout << aux->value << '\n';
+        aux = aux->next;
+    }
+    std::cout << '\n';
+}
+
+
+/*
+    Eliminamos primer nodo
+*/
+static void delete_first(StringList& l)
+{
+    if (l.first == nullptr)
+    {
+        return;
+    }
+    else
+    {
+        StringNode* aux = l.first;
+        l.first = l.first->next;
+        delete aux;
+
+        // Si first es nulo, no tenemos mas elementos
+        if (l.first == nullptr) l.last = nullptr;
+    }
+}
+
+
+int main()
+{
+    /*
+        Creo lista: "gordito", "noelia", "dani"
+    */
+    StringList sl{};
+    insert_first(sl, "noelia");
+    insert_first(sl, "gordito");
+    insert_last(sl, "dani");
+    print_list(sl);
+
+    /*
+        Meto y saco dani
+    */
+    insert_first(sl, "dani");
+    print_list(sl);
+
+    delete_first(sl);
+    print_list(sl);
 }
