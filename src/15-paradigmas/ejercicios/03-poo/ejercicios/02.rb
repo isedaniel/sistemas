4e45afeef3ce462e59ea6068f67253e63e24af81
Daniel Ise <ise.daniel@gmail.com>
2025-09-25 01:01:59 +0000

    Complete OOP Exercise 2
    Using Ruby to complete the second exercise of the OOP guide

--- src/15-paradigmas/ejercicios/03-poo/ejercicios/02.rb
+++ src/15-paradigmas/ejercicios/03-poo/ejercicios/02.rb
@@ -1,11 +1,38 @@
-class Room
-  def initialize furniture
-    @furniture = {}
-    furniture.each { |x| @furniture[x] = 1 }
+class Habitacion
+  def initialize muebles
+    @muebles = muebles
   end
 
+  def imprimir_muebles
+    @muebles.each { |mueble, cantidad| puts "#{mueble}: #{cantidad}" }
+  end
   attr_reader :furniture
 end
 
-r = Room.new ["bed", "chair"]
-puts r.furniture
+class Departamento
+  # este atajo permite crear getter y setters sin escrbir tanto en Ruby,
+  # mas o menos como también permite hacerlo C#
+  attr_reader :habitaciones
+
+  def initialize habitaciones
+    @habitaciones = habitaciones
+  end
+end
+
+# Creamos una habitacion con 2 camas y un placard, pasando un HashMap
+habitacion_principal = Habitacion.new({cama: 2, placard: 1})
+# Ruby permite usar unicode para definir variables, así que escribimos baño
+baño = Habitacion.new({inodoro: 1, bidet: 1, bañera: 1})
+
+# Creamos un departamento de una habitación
+depto = Departamento.new({habitacion: habitacion_principal, baño: baño})
+
+# Imprimimos muebles por consola
+depto.habitaciones[:habitacion].imprimir_muebles
+
+# Usamos :habitacion porque es un literal de Ruby que se llama "Symbol"
+# Internamente funciona como un entero y se usa para referirse a elementos 
+# en los HashMaps entre otras
+
+# Imprimimos mobiliario del baño
+depto.habitaciones[:baño].imprimir_muebles
