0b499c6cf7d56df745a15cc09f9c8c2430ee232e
Daniel Ise <ise.daniel@gmail.com>
2026-09-14 20:38:16 -0300

    Start tp5

---
+++ src/3/sistemas-operativos/tp5-cmd/directorios.bat
@@ -0,0 +1,14 @@
+md archivos
+cd archivos
+md adobe
+md adobe\ejercicios
+md adobe\manuales
+md bases
+md imagenes
+md imagenes\bmp
+md imagenes\jpg
+md imagenes\jpg\fotos
+md imagenes\otras
+md planillas
+md textos
+
